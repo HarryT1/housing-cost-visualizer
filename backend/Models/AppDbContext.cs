6dbc2e6b3dbc2a8c614b3f1773e8635ffc125644
Harry Thulin <harrythulin1@gmail.com>
2025-07-05 19:25:56 +0200

    Add files I accidentally forgot in previous commit

---
+++ backend/Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<PropertyListing> Properties { get; set; }
+}
---
+++ backend/Models/PropertyListing.cs
@@ -0,0 +1,18 @@
+namespace backend.Models;
+
+public class PropertyListing
+{
+    public int Id { get; set; }
+    public DateTime? SaleDate { get; set; }
+    public string? Municipality { get; set; }
+    public string? Neighborhood { get; set; }
+    public string? Address { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+    public string? SaleType { get; set; }
+    public int? Price { get; set; }
+    public double? AreaSqm { get; set; }
+    public double? Rooms { get; set; }
+    public double? Floor { get; set; }
+
+}
